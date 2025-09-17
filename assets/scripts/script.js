7db4c3d6fa8fd78cb0d62f764314d1d90b696e5d
Fabrizio <u201914181@upc.edu.pe>
2025-09-17 12:45:48 -0500

    feat(header): add styles and script for header section

---
+++ assets/scripts/script.js
@@ -0,0 +1,11 @@
+const navLinks = document.querySelectorAll('.navbar-nav .nav-link');
+
+
+navLinks.forEach(link => {
+    link.addEventListener('click', function () {
+
+        navLinks.forEach(nav => nav.classList.remove('active'));
+
+        this.classList.add('active');
+    });
+});
